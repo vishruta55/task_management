631b5ded60ea3f5a9e0c225550944d166a5f5d81
Kaviya110103 <kaviyamuthusamy11@gmail.com>
2026-06-12 16:38:19 +0530

    update render backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
     proxy: {
-      '/api': 'http://127.0.0.1:8000',
-      '/media': 'http://127.0.0.1:8000',
+      '/api': 'https://task-management-1-3zk8.onrender.com',
+      '/media': 'https://task-management-1-3zk8.onrender.com',
     },
   },
-});
+})
